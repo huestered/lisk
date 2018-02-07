4e3e63dbae75d0123950454ac9657f07bee953c0
Mona Baerenfaenger <mona@lightcurve.io>
2018-02-07 15:29:51 +0100

    :books: Standardize code documentation for helpers/api_error.js

--- helpers/api_error.js
+++ helpers/api_error.js
@@ -15,9 +15,13 @@
 
 /**
  * Extends standard Error with a code field and toJson function.
+ *
+ * @class
+ * @memberof helpers
  * @param {string} message
  * @param {number} code
- * @constructor
+ * @see Parent: {@link helpers}
+ * @todo Add descriptions of the params
  */
 function ApiError(message, code) {
 	this.message = message;
@@ -26,6 +30,11 @@ function ApiError(message, code) {
 
 ApiError.prototype = new Error();
 
+/**
+ * Description of the function.
+ *
+ * @todo Add returns-tag
+ */
 ApiError.prototype.toJson = function() {
 	return {
 		message: this.message,
